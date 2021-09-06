17c393bddcb4ba3e0781a4cc8077b2305a20b843
DavidBai <baiminghan@gmail.com>
2021-09-06 14:42:06 +1200

    2. finished the intro part.

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,38 @@
 import "./intro.scss";
+import ExpandMoreRoundedIcon from "@material-ui/icons/ExpandMoreRounded";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
-export default function intro() {
-  return <div className="intro" id="intro"></div>;
+export default function Intro() {
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ["Front End Developer", "UI/UX Designer", "Content Creator"],
+    });
+  });
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/developer-cartoon.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I am </h2>
+          <h1>David Bai</h1>
+          <h3>
+            Dynamic <span ref={textRef}></span>
+          </h3>
+          <a href="#portfolio">
+            <ExpandMoreRoundedIcon className="arrow-icon" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
